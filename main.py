1a4382366425aea969206f7dfcc12ad24639df52
camilo <chc201627@gmail.com>
2019-01-03 21:34:40 -0500

    Agregando funcion welcome

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ clients  = 'Camilo, Ernesto, '
 
 def create_client(client_name):
     global clients
-    clients += client_name
-    _add_comma()
+    if client_name not in clients:
+        clients += client_name
+        _add_comma()
+    else:
+        print('Client alreade is in the client\'s list')
 
 def list_clients():
     global clients
@@ -16,10 +19,24 @@ def  _add_comma():
     global clients
     clients += ','
 
+def _print_welcome():
+    print('WELCOME TO PLATZI VENTAS')
+    print ('*'*50)
+    print ('what would you like to do today ? ')
+    print ('[C]reate client')
+    print ('[D]elate client')
+
 
 if __name__ == "__main__":
-    list_clients()
-    
-    create_client('David')
-    
-    list_clients()
+
+    _print_welcome()
+
+    command = input()
+    if command == 'C':
+        client_name = input('What is the client name?')
+        create_client(client_name)
+        list_clients()
+    elif command =='D':
+        pass
+    else:
+        print('Invalid command')
