d3c4b35f69b7b1ee13e67fbe66ccbadcf1efe9c4
camilo <chc201627@gmail.com>
2019-01-03 21:08:35 -0500

    Agragando funciones create_clients

--- main.py
+++ main.py
@@ -2,7 +2,24 @@
 
 clients  = 'Camilo, Ernesto, '
 
+def create_client(client_name):
+    global clients
+    clients += client_name
+    _add_comma()
+
+def list_clients():
+    global clients
+
+    print(clients)
+
+def  _add_comma():
+    global clients
+    clients += ','
+
 
 if __name__ == "__main__":
-    clients += 'Felipe'
-    print(clients)
+    list_clients()
+    
+    create_client('David')
+    
+    list_clients()
