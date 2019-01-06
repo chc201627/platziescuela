05602f94691f6eeb987ecfa9a64b6351ed00e098
camilo <chc201627@gmail.com>
2019-01-05 19:27:18 -0500

    Agregando metodo delete

--- main.py
+++ main.py
@@ -9,11 +9,26 @@ def create_client(client_name):
         _add_comma()
     else:
         print('Client alreade is in the client\'s list')
+        
 
 def list_clients():
     global clients
 
     print(clients)
+
+def update_client(client_name, update_client_name):
+    global clients
+    if client_name in clients:
+        clients = clients.replace(client_name, update_client_name)
+    else:
+        _print_client_not_list()
+
+def delete_client(client_name):
+    global clients
+    if client_name in clients:
+        clients = clients.replace(client_name+',','')
+    else:
+       _print_client_not_list()
 
 def  _add_comma():
     global clients
@@ -24,19 +39,35 @@ def _print_welcome():
     print ('*'*50)
     print ('what would you like to do today ? ')
     print ('[C]reate client')
+    print ('[U]pdate client')
     print ('[D]elate client')
 
+def _get_client_name():
+    return input ('What is the client name?')
+
+def _print_client_not_list():
+    return   print('Client is not in client list')
 
 if __name__ == "__main__":
 
     _print_welcome()
 
     command = input()
+    command = command.upper()
+
+
     if command == 'C':
-        client_name = input('What is the client name?')
+        client_name = _get_client_name()
         create_client(client_name)
         list_clients()
     elif command =='D':
-        pass
+        client_name = _get_client_name()
+        delete_client(client_name)
+        list_clients()
+    elif command == 'U':
+        client_name = _get_client_name()
+        update_client_name=input('What is the updated client name? ')
+        update_client(client_name, update_client_name)
+        list_clients()
     else:
         print('Invalid command')
