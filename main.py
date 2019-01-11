4e4097cb0b7c2b250e4a5c309fb776b5a09d00ee
camilo <chc201627@gmail.com>
2019-01-10 20:24:12 -0500

    Agregando listas

--- main.py
+++ main.py
@@ -1,57 +1,53 @@
 import sys
 
-clients  = 'Camilo,Ernesto,Juan,'
+clients  = ['Camilo','Ernesto','Juan']
 
 def create_client(client_name):
     global clients
     if client_name not in clients:
-        clients += client_name
-        _add_comma()
+        clients.append(client_name)
+     
     else:
         print('Client alreade is in the client\'s list')
         
 
 def list_clients():
-    global clients
-
-    print(clients)
+    for idx, client in enumerate(clients):
+        print("{}:{}".format(idx, client))
 
 def update_client(client_name, update_client_name):
     global clients
     if client_name in clients:
-        clients = clients.replace(client_name, update_client_name)
+        index = clients.index(client_name)
+        clients[index] = update_client_name
     else:
         _print_client_not_list()
 
 def delete_client(client_name):
     global clients
     if client_name in clients:
-        clients = clients.replace(client_name+',','')
+        clients.remove(client_name)
     else:
        _print_client_not_list()
 
 def search_client(client_name):
-    
-    clients_list = clients.split(',')
-
-    for client in clients_list:
+    global clients  
+    for client in clients:
         if client != client_name:
             continue
         else:
             return True
 
 
-def  _add_comma():
-    global clients
-    clients += ','
 
 def _print_welcome():
     print('WELCOME TO PLATZI VENTAS')
     print ('*'*50)
     print ('what would you like to do today ? ')
-    print ('[C]reate client')
-    print ('[U]pdate client')
-    print ('[D]elate client')
+    print ('[C]reate clients')
+    print ('[L]ist clients ')
+    print ('[U]pdate clients')
+    print ('[D]elate clients')
     print ('[S]earch clients')
 
 def get_client_name():
@@ -84,6 +80,8 @@ if __name__ == "__main__":
     if command == 'C':
         client_name = get_client_name()
         create_client(client_name)
+        list_clients()
+    elif command == 'L':
         list_clients()
     elif command =='D':
         client_name = get_client_name()
